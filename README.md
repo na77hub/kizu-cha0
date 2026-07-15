f9bef80120c324c6ea2202125a1731398aed5e62
wktkdisk <waka@cyberbird17.local>
2026-07-15 14:24:36 +0900

    vite環境へ移行

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# kizu-cha0
+### kizu-cha0
+
+## 手順
+
+# 必要なモジュールのインストール
+npm install
+
+# VAPIDの取得
+node -e "const webpush=require('web-push'); const keys=webpush.generateVAPIDKeys(); console.log(JSON.stringify(keys,null,2));"
+
+出力結果を（以下のようなもの）、
+{
+  "publicKey": "BNxyz...（長い文字列）",
+  "privateKey": "abc123...（長い文字列）"
+}
+
+.env に入力する。
+
+
+
+
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+    root: 'src', // 開発ディレクトリ
+    base: './',
+    build: {
+        outDir: '../public', // ビルド成果物の出力先
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'src/index.html'),
+            },
+        },
+    },
+    server: {
+        port: 5173, // 開発時用（app.jsとは別ポート）
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+});
